1ec31a3d85c2e4ea90b09bd22be6d0ff69e07039
handshake2 <programerperu@gmail.com>
2018-08-30 19:10:24 -0500

    Initial Commit

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const http = require('http');
-const path = require('path');
+//const http = require('http');
+//const path = require('path');
 const express= require('express');
-const socketio = require('socket.io');
+//const socketio = require('socket.io');
 const app=express();
 
-const server=http.createServer(app);
-const io = socketio.listen(server);
+//const server=http.createServer(app);
+///const io = socketio.listen(server);
 
 app.set('port', process.env.PORT || 3000);
-require('./sockets')(io);
+//require('./sockets')(io);
 
 app.use(express.static('public'));
 
-server.listen(app.get('port'), () => {
+app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
   });
